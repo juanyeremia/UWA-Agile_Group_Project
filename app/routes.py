0db04a6c6b380ae646eeba16ccc38e07fdae5587
Thushamini Chathushika <66440838+chathushika2000@users.noreply.github.com>
2026-04-27 14:55:34 +0800

    admin_profile_and_footer

--- app/routes.py
+++ app/routes.py
@@ -14,3 +14,13 @@ def home():
 @app.route('/write_review')
 def write_review():
   return render_template('write_review.html')
+
+# Admin profile page route
+@app.route('/admin_profile')
+def admin_profile():
+  return render_template('admin_profile.html')
+
+# User profile page route
+@app.route('/user_profile')
+def user_profile():
+  return render_template('user_profile.html')
