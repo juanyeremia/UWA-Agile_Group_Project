47599574b075a282aa286648b049bf9c62d9d548
Tammy Weng <tammy@hiremii.com>
2026-05-06 01:49:50 +0800

    feat: Change the individual page, navbar, a some part of home page to fit with Flask structure

--- app/routes.py
+++ app/routes.py
@@ -12,9 +12,9 @@ def home():
   return render_template('home_page.html')
 
 # Write review page route
-@app.route('/write_review')
-def write_review():
-  return render_template('write_review.html')
+@app.route('/write_review/<int:movie_id>')
+def write_review(movie_id):
+    return render_template('write_review.html')
 
 @app.route('/sign_up')
 def sign_up():
@@ -31,3 +31,36 @@ def terms():
 @app.route('/privacy')
 def privacy():
   return render_template('privacy.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('user_profile.html')
+
+# Individual movie page for testing 
+@app.route('/movie/<int:movie_id>')
+def movie_detail(movie_id):
+    movie = {
+        "id": movie_id,
+        "title": "Arcane",
+        "poster_url": "https://img.league-funny.com/imgur/172482406314_o.png",
+        "release_date": "2024",
+        "director": "Riot Games",
+        "description": "This is a sample movie description.",
+        "cast": "Hailee Steinfeld, Ella Purnell",
+        "crew": "Created by Christian Linke and Alex Yee",
+        "details": "Animated series based on League of Legends.",
+        "genre": "Animation, Action, Adventure",
+        "release_info": "Released on Netflix"
+    }
+
+    reviews = [
+        {"user": {"username": "Tammy"}, "rating": 5, "content": "Amazing!"},
+        {"user": {"username": "Alex"}, "rating": 4, "content": "Pretty good!"}
+    ]
+
+    return render_template('individual_movie.html', movie=movie, reviews=reviews)
+
+""" - The original one after the test
+@app.route('/movie/<int:movie_id>')
+def movie_detail(movie_id):
+    return render_template('individual_movie.html') """
