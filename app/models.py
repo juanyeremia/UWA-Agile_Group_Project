26975cf7a6958c09884add6d5ee544fbd8b73171
Adrum <victor.kim.mobile@gmail.com>
2026-05-12 01:12:36 +0800

    1) Modified app/forms.py to add validation sign up form on backend 2) Modified app/routes.py to add flash message and redirect to login page after successful sign up 3) Modified requirements.txt to add Flask-Login package 4) Removed Forgot password link from login page

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 #=================================================
 # Define database models  
@@ -7,9 +8,15 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(80), unique=True, nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
-  password = db.Column(db.String(200), nullable=False)
+  password_hash = db.Column(db.String(200), nullable=False)
   role = db.Column(db.String(20), nullable=False, default='user')   # Role can be 'user' or 'admin'
   reviews = db.relationship('Review', back_populates='author')    # One-to-many relationship with Review
+  def set_password(self, password): # Hash the password and store it in the database
+    self.password_hash = generate_password_hash(password) 
+
+  def check_password(self, password): # Check the provided password against the stored hash
+    return check_password_hash(self.password_hash, password)
+
 
 class Review(db.Model):
   id = db.Column(db.Integer, primary_key=True)
